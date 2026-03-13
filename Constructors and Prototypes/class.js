6e464ab85919d8f3bd572d0010294418815f18da
Error_404 <built.different7813@gmail.com>
2026-03-13 10:53:41 +0530

    Add files via upload

---
+++ Constructors and Prototypes/class.js
@@ -0,0 +1,15 @@
+class Vehicle{
+    constructor(numWheels, price) {
+        this.numWheels = numWheels;
+        this.price = price;
+    }
+    getPrice() {
+        return this.price;
+    }
+}
+
+var vehicle1 = new Vehicle(2, 50000);
+var vehicle2 = new Vehicle(4, 100000);
+
+console.log(vehicle1);
+console.log(vehicle2);
